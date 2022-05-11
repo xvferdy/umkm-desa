ca958bb148241328676f4d1c2892d5ee1d312c91
xvferdy <xv.ferdy@gmail.com>
2022-05-12 00:25:49 +0800

    💄 [UI]: input email full width

--- components/Order.jsx
+++ components/Order.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const Order = () => {
-  const [value, setValue] = React.useState(null);
+  const [value, setValue] = useState(null);
+
+  // const [date, setDate] = useState("");
+  const [email, setEmail] = useState("");
+  const [err, setErr] = useState(false);
+  console.log(value);
+
+  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  const handleSubmit = (params) => {
+    // if (value === null || err) return alert("error");
+
+    return alert("booked!");
+  };
   return (
     <div className="order">
       <div className="container order__container">
         <form className="order__form">
-          <TextField id="outlined-basic" label="Guest" variant="outlined" />
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setErr(Boolean(!email.match(pattern)));
+            }}
+            error={err}
+          />
+
           <LocalizationProvider dateAdapter={AdapterDateFns}>
             <DatePicker
-              label="Reservation Date"
+              label="Date"
               value={value}
               onChange={(newValue) => {
                 setValue(newValue);
@@ -21,7 +46,11 @@ const Order = () => {
               renderInput={(params) => <TextField {...params} />}
             />
           </LocalizationProvider>
-          <button className="btn btn--primary" type="button">
+          <button
+            className="btn btn--primary"
+            type="button"
+            onClick={handleSubmit}
+          >
             Book
           </button>
         </form>
