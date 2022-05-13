c136b2b9cfb762c1c766eb773ee69dbe069fe4a7
xvferdy <xv.ferdy@gmail.com>
2022-05-13 09:48:17 +0800

    ➖ [package]: remove mongodb

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,6 @@
 import Link from "next/link";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 
 const NotFound = () => {
-  const router = useRouter();
-
-  // useEffect(() => {
-  //   let timer = setTimeout(() => {
-  //     router.push("/");
-  //   }, 3000);
-  //   return () => clearTimeout(timer);
-  // }, []);
-
   return (
     <section className="not-found">
       <h1>Ooops...</h1>
